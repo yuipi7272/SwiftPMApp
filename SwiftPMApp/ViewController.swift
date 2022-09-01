fa17c07c2e4f3c139497c9af8065187984c5a6e9
YuiOgawa <yui20010218@gmail.com>
2022-09-01 11:06:10 +0900

    Initial Commit

---
+++ SwiftPMApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftPMApp
+//
+//  Created by Yui Ogawa on 2022/09/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
